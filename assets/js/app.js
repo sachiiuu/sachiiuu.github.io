3c1c1ad615c368ed58aa4a8fba7a1cf49216279c
sachikoniki <85739968+sachikoniki@users.noreply.github.com>
2023-07-03 23:11:16 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -96,7 +96,6 @@ slider.addEventListener("mousemove", (e) => {
   const x = e.pageX - slider.offsetLeft;
   const walk = (x - startX) * 3; //scroll-fast
   slider.scrollLeft = scrollLeft - walk;
-  console.log(walk);
 });
 
 const mouseWheel = document.querySelector(".items");
